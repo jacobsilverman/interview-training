f79e26836a2e85da2f4e672f0b11e79bc4c64813
Jacob Silverman <57313642+jacobsilverman@users.noreply.github.com>
2022-11-15 21:03:15 -0800

    building page basic asthetic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import './App.css'
+import "./App.css";
+import { questions } from "./Constants/Questions.jsx";
+import { Paper } from '@mui/material';
 
 function App() {
+  const questionAnswer = (obj) => {
+    return Object.entries(obj).map(([innerKey, value]) => {
+      return (
+        <Paper className="paper-element" elevation={9} key={innerKey}>
+          <h3>{value.question()}</h3>
+          <div className="content">{value.answer()}</div>
+        </Paper>
+      )
+    })
+  }
+
+  const display = () => {
+    return (
+      Object.keys(questions).map((key) => {
+        return (
+          <div key={key}>
+            <h2 className="topic">{key}</h2>
+            {questionAnswer(questions[key])}
+          </div>
+        )
+      })
+    );
+  }
 
   return (
     <div className="App">
-     
+      <header>
+        <h1>Interview Questions</h1>
+      </header>
+      {display()}
     </div>
   )
 }
---
+++ src/Constants/Questions.jsx
@@ -0,0 +1,80 @@
+import "./Questions.css";
+
+
+export const questions = {
+    "Javascript" : {
+        0: {
+            "question": function () {
+
+                return (
+                    <div className="question1">
+                        <span>difference between <span className="highlight">async</span> and <span className="highlight">defer</span> in the <span className="highlight">&lt;script&gt;</span> tag and if none are used</span>
+                    </div>
+                )
+            },
+            "answer": function () {
+                const part1 = `<script src='file.js'></script>`
+                const part2 = `<script src='file.js' defer></script>`;
+                const part3 = `<script src='file.js' async></script>`;
+                return (
+                    <>
+                        <div><b>1.</b> excuted synschronously, neither can be used inline, need src. {part1}</div>
+                        <div><b>2.</b> defer: downloads the script while the document is stilling parsing, but waits till it finishes parsing before executing it. Scripts rely on eachother use DOMContentLoaded event listener is the same. {part2}</div>
+                        <div><b>3.</b> async: downloads the script during parsing the doc but will pause the parser to execute the script before it finishes parsing. Doesn't neccessarily execute in order. Use if scripts are independent of eachother. {part3}</div>
+                    </>
+                );
+            }
+        }
+    },
+    "React": {
+        0: {
+            "question": function () {
+                return (<div>What is the virtual DOM</div>)
+            },
+            "answer":  function() {
+                return (<div>not the DOM</div>);
+            }
+            
+        }
+    },
+    "CSS": {
+        0: {
+            "question": function () {
+                return (<div>types of @ media properties</div>)
+            },
+            "answer": function() {
+                const example = `<link rel="stylesheet" media="screen and (max-width: 600px)`;
+                return (
+                    <>
+                        <div><b>all:</b> applies to all media type devices</div>
+                        <div><b>print:</b> applies only to printers</div>
+                        <div><b>screen:</b> applies to screens(desktop, tablets, mobile, etc)</div>
+                        <div><b>speech:</b> screen readers</div>
+                        <div><b>exmaple:</b> {example}</div>
+                    </>
+                );
+            }
+            
+        }
+    },
+    "HTML": {
+        0: {
+            "question": function () {
+                return (<div>describe and element</div>)
+            },
+            "answer": function () {
+                return (<div>HTML</div>);
+            }
+        }
+    },
+    "NETWORK": {
+        0: {
+            "question": function() {
+                return (<div>What is CORS</div>);
+            },
+            "answer": function() {
+                return (<div>Cross origin resource sharing</div>)
+            }
+        }
+    }
+};
